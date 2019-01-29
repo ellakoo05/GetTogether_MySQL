e955d2e65c3151dac4b9805fb6129a25979ad372
ellakoo05 <ella@github.com>
2019-01-29 14:25:04 -0800

    2nd commit
    deleted port

--- mysql/selectuser.php
+++ mysql/selectuser.php
@@ -5,10 +5,8 @@ $hostname = "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "sbib1pmzgwmexs5g"; 
 $password = "tk4m14h6yyt889va";
 $dbname = "fvtmkfyz8ymzy8a1";
-$port = "3306";
-
 // Check connection
-$conn = mysqli_connect($hostname, $username, $password, $dbname, $port);
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
 die("Connection failed: " . mysqli_connect_error());
