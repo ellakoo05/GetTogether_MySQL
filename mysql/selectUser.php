7cc2935e7c83652e1ab30dd6fc38a1c141f45e9f
ellakoo05 <ella@github.com>
2019-01-30 21:45:31 -0800

    selectUser created

---
+++ mysql/selectUser.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+
+try {
+    $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
+}
+catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$username     = $_POST['username'];
+$email     = $_POST['email'];
+$password = $_POST['password'];
+
+//$query = "SELECT * FROM users";
+$query = "SELECT * FROM users";
+
+$result = $conn->query($query);
+if ($result) {
+    $users = $result->fetchAll();
+    echo json_encode($users);
+} else {
+    echo json_encode(false);
+}
+?>
