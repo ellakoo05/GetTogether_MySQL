93352f29dc317be3ef66d6cbae9466c5bd19928e
ellakoo05 <ella@github.com>
2019-01-30 18:50:47 -0800

    Update mysql/selectEvent.php

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -6,24 +6,23 @@ header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
     $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
 }
-catch(PDOException $e)
-{
-    echo "Error: " .$e->getMessage();
+catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 
-$eventname=$_POST['eventname']
-$eventdate =$_POST['eventdate'];
-$eventlocation =$_POST['eventlocation'];
-$eventtime =$_POST['eventtime'];
+$eventname     = $_POST['eventname'];
+$eventdate     = $_POST['eventdate'];
+$eventlocation = $_POST['eventlocation'];
+$eventtime     = $_POST['eventtime'];
 
 //$query = "SELECT * FROM users";
 $query = "SELECT * FROM events WHERE eventname='$eventname', eventdate='$eventdate', eventlocation='$eventlocation', AND eventtime='$eventtime'";
 
 $result = $conn->query($query);
-if($result){
-$users = $result->fetchAll();
-echo json_encode($users);
+if ($result) {
+    $events = $result->fetchAll();
+    echo json_encode(true);
 } else {
-    echo json_encode (false);
+    echo json_encode(false);
 }
 ?>
