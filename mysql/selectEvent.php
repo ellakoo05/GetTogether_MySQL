8efff67103b79574393afa1e487716a49196d1b6
ellakoo05 <ella@github.com>
2019-01-30 18:25:57 -0800

    Update mysql/selectEvent.php

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -4,7 +4,7 @@
 //header('Access-Control-Allow-Methods: GET, POST');
 //header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: https://developer.mozilla.org');
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
         header('Access-Control-Allow-Headers: token, Content-Type');
         header('Access-Control-Max-Age: 1728000');
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         die();
     }
 
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: https://developer.mozilla.org');
     header('Content-Type: application/json');
 
     $ret = [
