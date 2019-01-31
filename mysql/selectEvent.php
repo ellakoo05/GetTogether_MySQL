0805b6f2e5b5522c5725d34f0ffa60bc6d6fca2e
ellakoo05 <ella@github.com>
2019-01-30 18:41:04 -0800

    back to normal

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -1,28 +1,7 @@
 <?php
-//header("Access-Control-Allow-Origin: *");
-//header("Access-Control-Allow-Methods: GET, POST");
-//header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
-
-if($_SERVER['HTTP_ORIGIN'] == "http://gettogetherbcit.herokuapp.com/mysql/selectEvent.php") {
-    header('Access-Control-Allow-Origin: http://gettogetherbcit.herokuapp.com/mysql/selectEvent.php');
-    header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
-} else {    
-  header('Content-Type: text/html');
-  echo "<html>";
-  echo "<head>";
-  echo "   <title>Another Resource</title>";
-  echo "</head>";
-  echo "<body>",
-       "<p>This resource behaves two-fold:";
-  echo "<ul>",
-         "<li>If accessed from <code>http://arunranga.com</code> it returns an XML document</li>";
-  echo   "<li>If accessed from any other origin including from simply typing in the URL into the browser's address bar,";
-  echo   "you get this HTML document</li>", 
-       "</ul>",
-     "</body>",
-   "</html>";
-}
 
 try {
     $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
