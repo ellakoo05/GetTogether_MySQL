538f9ef83ff2e6e336d8908064f007f7d3e9e889
ellakoo05 <ella@github.com>
2019-01-30 18:06:00 -0800

    added headers

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
 try {
     $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
