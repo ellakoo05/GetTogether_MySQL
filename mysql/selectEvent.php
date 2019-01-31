4b620dcd38fb84fdd43924f461623e6215dd13c5
ellakoo05 <ella@github.com>
2019-01-30 18:32:36 -0800

    Update mysql/selectEvent.php

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -1,25 +1,24 @@
 <?php
-//header('Access-Control-Allow-Origin: https://developer.mozilla.org
-//Vary: Origin');
-//header('Access-Control-Allow-Methods: GET, POST');
-//header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        header('Access-Control-Allow-Origin: https://developer.mozilla.org');
-        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-        header('Access-Control-Allow-Headers: token, Content-Type');
-        header('Access-Control-Max-Age: 1728000');
-        header('Content-Length: 0');
-        header('Content-Type: text/plain');
-        die();
-    }
-
-    header('Access-Control-Allow-Origin: https://developer.mozilla.org');
-    header('Content-Type: application/json');
-
-    $ret = [
-        'result' => 'OK',
-    ];
-    print json_encode($ret);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+//if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//        header('Access-Control-Allow-Origin: https://developer.mozilla.org');
+//        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+//        header('Access-Control-Allow-Headers: token, Content-Type');
+//        header('Access-Control-Max-Age: 1728000');
+//        header('Content-Length: 0');
+//        header('Content-Type: text/plain');
+//        die();
+//    }
+//
+//    header('Access-Control-Allow-Origin: https://developer.mozilla.org');
+//    header('Content-Type: application/json');
+//
+//    $ret = [
+//        'result' => 'OK',
+//    ];
+//    print json_encode($ret);
 
 try {
     $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
