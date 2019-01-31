dcbe441860db549a4052813d7882222e78bda1e1
ellakoo05 <ella@github.com>
2019-01-30 18:11:51 -0800

    deleted * on header

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://developer.mozilla.org
+Vary: Origin');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
