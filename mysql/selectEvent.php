9ba5bd561d70d46f0384313eeacc0cba7c0e748c
ellakoo05 <ella@github.com>
2019-02-07 13:27:35 -0800

    select_Event change

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -3,6 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
+$data = json_decode(file_get_contents("php://input"),true);
+
 try {
     $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
 }
@@ -14,9 +16,10 @@ $eventname     = $_POST['eventname'];
 $eventdate     = $_POST['eventdate'];
 $eventlocation = $_POST['eventlocation'];
 $eventtime     = $_POST['eventtime'];
+$eventID = $data['id'];
 
 //$query = "SELECT * FROM users";
-$query = "SELECT * FROM events";
+$query = "SELECT * FROM events WHERE id='".$eventID."';";
 
 $result = $conn->query($query);
 if ($result) {
