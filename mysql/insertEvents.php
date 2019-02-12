a6e3ff8e334d1f97f29d1fd30cef6175bb6e54f1
ellakoo05 <ella@github.com>
2019-02-11 19:33:11 -0800

    1

--- mysql/insertEvents.php
+++ mysql/insertEvents.php
@@ -1,24 +1,23 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
-$hostname = "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"; 
-$username = "sbib1pmzgwmexs5g"; 
+$hostname = "tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "sbib1pmzgwmexs5g";
 $password = "tk4m14h6yyt889va";
-$dbname = "fvtmkfyz8ymzy8a1";
-$port = "3306";
+$dbname   = "fvtmkfyz8ymzy8a1";
+$port     = "3306";
 // Check connection
-$conn = mysqli_connect($hostname, $username, $password, $dbname, $port);
+$conn     = mysqli_connect($hostname, $username, $password, $dbname, $port);
 // Check connection
 if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
-}
-else {
-  $sql = "INSERT INTO events (eventname, eventdate, eventtime, eventlocation, eventend, eventenddate) VALUES ('{$_POST['eventname']}','{$_POST['eventdate']}','{$_POST['eventtime']}','{$_POST['eventlocation']}','{$_POST['eventend']}','{$_POST['eventenddate']}')";
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    $sql = "INSERT INTO events (eventname, eventdate, eventtime, eventlocation, eventend, eventenddate) VALUES ('{$_POST['eventname']}','{$_POST['eventdate']}','{$_POST['eventtime']}','{$_POST['eventlocation']}','{$_POST['eventend']}','{$_POST['eventenddate']}')";
 }
 
-$res2 = mysqli_query($conn,$sql);
+$res2 = mysqli_query($conn, $sql);
 //echo $res2;
 //if($res2 === TRUE){
 //echo "New record created successfully"; 
