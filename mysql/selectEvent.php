042930f8894c996e50efd3339cb875c260c25947
ellakoo05 <ella@github.com>
2019-02-12 01:02:32 -0800

    Update selectEvent.php

--- mysql/selectEvent.php
+++ mysql/selectEvent.php
@@ -25,15 +25,9 @@ $result = $conn->query($query);
 if ($result) {
     $events = $result->fetchAll();
     if (!empty($events)) {
-        echo json_encode(array(
-            "status" => true,
-            "id" => $events[0]["id"],
-            "eventname" => $eventname,
-            "eventdate" => $eventdate,
-            "eventlocation" => $eventlocation
-        ));
-    } else {
         echo json_encode($events);
+    } else {
+        echo json_encode(false);
     }
 } else {
     echo json_encode(false);
