d24bdef05b5824a46af30ef535bc25c40023dc28
ellakoo05 <ella@github.com>
2019-02-24 21:20:24 -0800

    delete event fixed

---
+++ mysql/deleteEvents.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+try {
+    $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
+}
+catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$eventCode = $_POST['eventCode'];
+
+$query = "DELETE FROM events WHERE eventCode='$eventCode'";
+
+$result = $conn->query($query);
+
+if ($result) {
+    $events = $result->fetchAll();
+    if (!empty($events)) {
+        echo json_encode($events);
+    } else {
+        echo json_encode(false);
+    }
+} else {
+    echo json_encode(false);
+}
+?>
---
+++ mysql/updateTasks.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+try {
+    $conn = new PDO("mysql:host=tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fvtmkfyz8ymzy8a1", "sbib1pmzgwmexs5g", "tk4m14h6yyt889va");
+}
+catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$userID     = $_POST['userID'];
+$eventCode     = $_POST['eventCode'];
+$tasks       = $_POST['tasks'];
+
+$query = "UPDATE tasks SET (userID, eventCode, tasks) VALUES ('{$_POST['userID']}','{$_POST['eventCode']}','{$_POST['tasks']}')";
+$result = $conn->query($query);
+if ($result) {
+    $users = $result->fetchAll();
+    echo json_encode($users);
+} else {
+    echo json_encode(false);
+}
+?>
