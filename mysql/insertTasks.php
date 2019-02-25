531a2e418b6f8ca1180fe44b2c52f2653b815b24
ellakoo05 <ella@github.com>
2019-02-25 03:00:37 -0800

    Update insertTasks.php

--- mysql/insertTasks.php
+++ mysql/insertTasks.php
@@ -19,9 +19,9 @@ $query = "INSERT INTO tasks (eventCode, tasks) VALUES ('$eventCode', '$tasks')";
 $result = $conn->query($query);
 if ($result) {
     $tasks = $result->fetchAll();
+  $conn->insert_id;
   $last_id=$conn->insert_id;
-  error_log($last_id);
-    echo json_encode($tasks);
+    echo json_encode($last_id);
 } else {
     echo json_encode(false);
 }
