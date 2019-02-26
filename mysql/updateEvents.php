8daf73d709d6452357e3ff650e8a5d272ff6bba4
ellakoo05 <ella@github.com>
2019-02-25 18:51:59 -0800

    Update updateEvents.php

--- mysql/updateEvents.php
+++ mysql/updateEvents.php
@@ -11,17 +11,16 @@ catch (PDOException $e) {
 $eventname     = $_POST['eventname'];
 $eventdate     = $_POST['eventdate'];
 $eventlocation       = $_POST['eventlocation'];
-$eventnote = $_POST['eventnote'];
 $eventenddate = $_POST['eventenddate'];
 $eventtime = $_POST['eventtime'];
 $eventend = $_POST['eventend'];
-$eventCode = $_POST['eventCode']
 
-$query = "UPDATE events SET (eventname, eventdate, eventlocation, eventnote, eventenddate, eventtime, eventend, eventCode) VALUES ('{$_POST['eventname']}','{$_POST['eventdate']}','{$_POST['eventlocation']}', '{$_POST['eventnote']}', '{$_POST['eventenddate']}', '{$_POST['eventtime']}', '{$_POST['eventend']}', '{$_POST['eventCode']}')";
+$query = "UPDATE events SET (eventname, eventdate, eventlocation, eventnote, eventenddate, eventtime, eventend, eventCode) VALUES ('$eventname','$eventdate','$eventlocation', '$eventenddate', '$eventtime', '$eventend')";
+
 $result = $conn->query($query);
 if ($result) {
-    $users = $result->fetchAll();
-    echo json_encode($users);
+    $events = $result->fetchAll();
+    echo json_encode($events);
 } else {
     echo json_encode(false);
 }
