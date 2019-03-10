22aa31c370acc40ee812879c56bb497d4733b572
ellakoo05 <ella@github.com>
2019-03-09 18:43:03 -0800

    Update getAssignedUsers.php

--- mysql/getAssignedUsers.php
+++ mysql/getAssignedUsers.php
@@ -12,24 +12,19 @@ catch (PDOException $e) {
 
 $eventCode = $_POST['eventCode'];
 
-$query = "SELECT * FROM events INNER JOIN tasks ON events.eventCode=tasks.eventCode WHERE events.eventCode='$eventCode'";
+$query = "SELECT * FROM assigned_tasks WHERE eventCode='$eventCode'";
 
 $result = $conn->query($query);
 
 if ($result) {
-    $eventTasks = $result->fetchAll();
-  if(!empty($eventTasks)){ 
-    $eventTasksArray = array();
-    for ($i=0;$i<sizeof($eventTasks);$i++) {
-      array_push($eventTasksArray, array(
-    "id"=>$eventTasks[$i]["id"],
-    "tasks"=>$eventTasks[$i]["tasks"],
-    ));
+    $assigned_users = $result->fetchAll();
+    if (!empty($assigned_users)) {
+        echo json_encode($assigned_users);
+    } else {
+        echo json_encode(false);
     }
-    echo json_encode($eventTasksArray);
-  } else {
-    echo json_encode($eventTasks);
-}} else {
+} else {
     echo json_encode(false);
 }
+?>
 ?>
